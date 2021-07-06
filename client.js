e7ae8c257d4365c30d19f74d5a2130f01022c44f
qtsun <838280705@qq.com>
2021-07-06 22:30:04 +0800

    前三节

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require('net')
+const parser = require('./parser.js')
 
 class Request {
     constructor(options){
@@ -204,6 +205,8 @@ void async function (){
 
     let response = await request.send()
 
-    console.log(response)
+    let dom = parser.parseHTML(response.body)
+
+    console.log(response, dom)
 }()
 
--- server.js
+++ server.js
@@ -1,24 +1,53 @@
 const http = require('http')
 
-http.createServer((request, response) => {
-    let body = []
-    request.on('error', (err) => {
-        console.error(err)
-    }).on('data', (chunk) => {
-        // let b = Buffer(chunk)
-        // let u = new Uint8Array(b.length)
-        // for(let i = 0; i < b.length; i++){
-        //     u[i] = b[i]
-        // }
-        // body.push(u)
-        body.push(chunk.toString())
-    }).on('end', ()=>{
-        body = body.join("")
-        // body = Buffer.concat(body).toString()
-        console.log('body:', body)
-        response.writeHead(200, {'Content-Type': 'text/html'})
-        response.end(' Hello World\n')
-    })
-}).listen(8088)
+const server = http.createServer((req, res) => {
+    console.log('request received')
+    console.log(req.headers)
+    res.setHeader('Content-Type', 'text/html')
+    res.setHeader('X-Foo', 'bar')
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    res.end(
+`<html maaa=a >
+<head>
+    <style>
+body div #myid{
+    width:100px;
+    background-color: #ff5000;
+}
+body div img{
+    width:30px;
+    background-color: #ff1111
+}
+    </style>
+</head>
+<body>
+    <div>
+        <img id="myid" />
+        <img />
+    </div>
+</body>
+</html>`)
+})
+server.listen(8088)
+// http.createServer((request, response) => {
+//     let body = []
+//     request.on('error', (err) => {
+//         console.error(err)
+//     }).on('data', (chunk) => {
+//         // let b = Buffer(chunk)
+//         // let u = new Uint8Array(b.length)
+//         // for(let i = 0; i < b.length; i++){
+//         //     u[i] = b[i]
+//         // }
+//         // body.push(u)
+//         body.push(chunk.toString())
+//     }).on('end', ()=>{
+//         body = body.join("")
+//         // body = Buffer.concat(body).toString()
+//         console.log('body:', body)
+//         response.writeHead(200, {'Content-Type': 'text/html'})
+//         response.end(' Hello World\n')
+//     })
+// }).listen(8088)
 
-console.log('server started')
+// console.log('server started')
