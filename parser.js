a301a387e59574e656635132fcabe2e208f752f0
qtsun <838280705@qq.com>
2021-07-08 10:12:30 +0800

    HTML创建元素和处理属性

--- parser.js
+++ parser.js
@@ -1,7 +1,7 @@
 const EOF = Symbol("EOF") //EOF: End Of File
 
 let currentToken = null
-
+let currentAttribute = null
 function emit(token) {
     console.log(token)
 }
@@ -67,18 +67,138 @@ function tagName(c) {
 function beforeAttributeName(c) {
     if(c.match(/^[\t\n\f ]$/)) {
         return beforeAttributeName
-    } else if(c == '>') {
-        return data
+    } else if( c == '/' || c == '>' || c == EOF) {
+        return afterAtrributeName(c)
     } else if (c == '=') {
-        return beforeAttributeName
+
     } else {
-        return beforeAttributeName
+        currentAttribute = {
+            name:'',
+            value:''
+        }
+        return attributeName(c)
     }
 }
+
+function attributeName(c) {
+    if(c.match(/^[\t\n\f ]$/) || c == '/' || c == '>' || c == EOF) {
+        return afterAtrributeName(c)
+    } else if(c == '=') {
+        return beforeAttributeValue
+    } else if(c == '\"' || c == "'" || c == "<") {
+
+    } else {
+        currentAttribute.name += c
+        return attributeName
+    }
+}
+
+function beforeAttributeValue(c) {
+    if(c.match(/^[\t\n\f ]$/) || c == '/' || c == ">" || c == EOF) {
+        return beforeAttributeValue
+    } else if(c == "\"") {
+        return doubleQuotedAttributeValue
+    } else if(c == "\'") {
+        return singleQuotedAttributeValue
+    } else if(c == ">") {
+
+    } else {
+        return UnquotedAttributeValue(c)
+    }
+}
+function doubleQuotedAttributeValue(c) {
+    if(c == "\"") {
+        currentToken[currentAttribute.name] = currentAttribute.value
+        return afterQuotedAttributeValue
+    } else if(c == "\u0000") {
+
+    } else if(c == EOF) {
+
+    } else {
+        currentAttribute.value += c
+        return doubleQuotedAttributeValue
+    }
+}
+function afterQuotedAttributeValue(c){
+    if(c.match(/^[\t\n\f ]$/)) {
+        return beforeAttributeName
+    } else if(c == "/") {
+        return selfClosingStartTag
+    } else if(c == ">") {
+        currentToken[currentAttribute.name] = currentAttribute.value
+        emit(currentToken)
+        return data
+    } else if(c == EOF) {
+
+    } else {
+        currentAttribute.value += c
+        return doubleQuotedAttributeValue
+    }
+}
+function UnquotedAttributeValue(c) {
+    if(c.match(/^[\t\n\f ]$/)) {
+        currentToken[currentAttribute.name] = currentAttribute.value
+        return beforeAttributeName
+    } else if (c == "/") {
+        currentToken[currentAttribute.name] = currentAttribute.value
+        return selfClosingStartTag
+    } else if(c == ">") {
+        currentToken[currentAttribute.name] =  currentAttribute.value
+        emit(currentToken)
+        return data
+    } else if(c == "\u0000") {
+
+    } else if(c == "\"" || c =="'" || c == "<" || c == "=" || c == "`") {
+
+    } else if(c == EOF) {
+
+    } else {
+        currentAttribute.value += c
+        return UnquotedAttributeValue
+    }
+}
+
+function singleQuotedAttributeValue(c) {
+    if(c == "\'") {
+        currentToken[currentAttribute.name] = currentAttribute.value
+        return afterQuoted
+    } else  if(c == EOF) {
+
+    } else {
+        currentAttribute.value += c
+        return doubleQuotedAttributeValue
+    }
+}
+
+function afterAtrributeName(c) {
+    if(c.match(/^[\t\n\f ]$/)) {
+        return afterAtrributeName
+    } else if (c == "/") {
+        return selfClosingStartTag
+    }else if(c == "="){
+        return beforeAttributeValue
+    } else if(c == ">") {
+        currentToken[currentAttribute.name] = currentAttribute.value
+        emit(currentToken)
+        return data
+    } else if(c == EOF) {
+
+    } else {
+        currentToken[currentAttribute.name] = currentAttribute.value
+        currentAttribute = {
+            name: "",
+            value: ""
+        }
+        return attributeName(c)
+    }
+}
+
 
 function selfClosingStartTag(c) {
     if (c == '>') {
         currentToken.isSelfClosing = true
+        emit(currentToken)
+        return data
     } else if(c == 'EOF') {
 
     } else {
