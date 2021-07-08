85ca59a81dc4608948abddda1eb82e7de4e49864
qtsun <838280705@qq.com>
2021-07-08 11:42:05 +0800

    HTML解析|用Token构建按DOM树|将文本节点加到DOM树

--- parser.js
+++ parser.js
@@ -1,21 +1,72 @@
-const EOF = Symbol("EOF") //EOF: End Of File
-
 let currentToken = null
 let currentAttribute = null
+
+let stack = [{type:"document", children:[]}]
+
+let currentTextNode = null
+
+const EOF = Symbol("EOF") //EOF: End Of File
+
 function emit(token) {
-    console.log(token)
+   
+    let top = stack[stack.length - 1]
+
+    if(token.type == "startTag") {
+        let element = {
+            type: "element",
+            children: [],
+            attributes: []
+        }
+
+        element.tagName = token.tagName
+        for(let p in token) {
+            if(p != "type" && p != "tagName")
+                element.attributes.push({
+                    name: p,
+                    value: token[p]
+                })
+        }
+
+        top.children.push(element)
+        element.parent = top
+
+        if(!token.isSelfClosing)
+            stack.push(element)
+        
+        currentTextNode = null
+
+    } else if(token.type == "endTag") {
+        if(top.tagName != token.tagName) {
+            throw new Error("Tag start end doesn't match!")
+        } else {
+            stack.pop()
+        }
+        currentTextNode = null
+    } else if(token.type == "text") {
+        if(currentTextNode == null) {
+            currentTextNode = {
+                type: "text",
+                content: ""
+            }
+            top.children.push(currentTextNode)
+        }
+        currentTextNode.content += token.content
+    }
 }
 
 function data(c){
     if(c == '<') {
         return tagOpen
-    } else if(c == EOF) {
+    } else if( c == EOF){
+        emit({
+            type:'EOF',
+        })
+        return
+    } else {
         emit({
             type:'text',
             content:c
         })
-        return
-    } else {
         return data
     }
 }
@@ -212,4 +263,5 @@ module.exports.parseHTML = function parseHTML(html){
         state = state(c)
     }
     state = state(EOF)
+    return stack[0]
 }
